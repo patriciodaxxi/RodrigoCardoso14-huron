f6246d679ffcd554715e66b4359ba06dfd724796
Rodrigo Cardoso <ro.drigocardoso@hotmail.com>
2019-10-13 18:26:43 -0300

    First Commit

---
+++ Huron.dpr
@@ -0,0 +1,14 @@
+program Huron;
+
+uses
+  Vcl.Forms,
+  View.Principal in 'View\View.Principal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
